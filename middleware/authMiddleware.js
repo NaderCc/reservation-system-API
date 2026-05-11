59205f3dd0add542b87276793d726db721182cc0
NaderCc <kanmagdy@gmail.com>
2026-05-11 15:14:16 +0300

    feat(middleware):add middleware files

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = process.env.JWT_SECRET || 'super_secret_key_2026';
+
+module.exports = (req, res, next) => {
+    const authHeader = req.header('Authorization');
+    const token = authHeader && authHeader.split(' ')[1];
+    if (!token) return res.status(401).json({ error: 'No token, authorization denied' });
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        res.status(401).json({ error: 'Token is not valid' });
+    }
+};
