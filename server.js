c48abd6eef4bfd625720cd156864261f71388970
NaderCc <kanmagdy@gmail.com>
2026-06-01 11:41:59 +0300

    try jenkins

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 100,
+    max: 95,
     message: 'Too many requests from this IP, please try again later.',
 });
 
